6d624f8dfceaf0f926021400ebc9ec841414a24e
hienhoang <51957758+giahienhoang99@users.noreply.github.com>
2024-11-09 15:58:16 -0500

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState, useRef } from 'react';
-import { Textarea } from "@/components/ui/textarea";
+import { Textarea } from "@/components/ui/textarea"
 import { ArrowUp } from "lucide-react";
 import { Button } from '@/components/ui/button';
 import { useNavigate } from 'react-router-dom';
-import { CardWithForm } from '@/components/Card';
 import axios from 'axios';
 
 const Home = () => {
@@ -11,7 +10,7 @@ const Home = () => {
   const [message, setMessage] = useState('');
   const [response, setResponse] = useState([]);
   const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -36,11 +35,11 @@ const Home = () => {
         const res = await axios.post('http://127.0.0.1:8000/message', {
           message: message.trim(),
         });
-        console.log(res.data);
+        //console.log(res.data);
         
         // set the response from the backend (for display or further use)
         setResponse(res.data);
-        //console.log(response);
+        console.log(response);
 
         // clear the message field
         setMessage('');
@@ -57,13 +56,33 @@ const Home = () => {
           isVisible ? 'opacity-100 transition-opacity duration-1000' : 'opacity-0'
         }`}
       >
-        <div className="text-center mb-10">
+        <div className="text-center mb-8">
           <h1 className="text-7xl font-bold mb-4">researchAI</h1>
           <p className="text-xl">Your AI Paper Research</p>
         </div>
         
-        <div className='flex justify-center w-full'>
-          <CardWithForm />
+        <div className="relative flex flex-col w-full">
+          <Textarea 
+            ref={textareaRef}
+            placeholder="Type your message here." 
+            className="min-h-[52px] max-h-[200px] rounded-3xl pr-14 resize-none overflow-y-auto py-3.5 leading-7"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' && !e.shiftKey) {
+                e.preventDefault();
+                handleSubmit();
+              }
+            }}
+            rows={1}
+          />
+          <Button 
+            className="absolute right-3 bottom-3 h-7 w-7 bg-white rounded-full p-0 hover:bg-slate-700"
+            variant="ghost"
+            onClick={handleSubmit}
+          >
+            <ArrowUp className="h-4 w-4 text-black" />
+          </Button>
         </div>
       </div>
     </div>
