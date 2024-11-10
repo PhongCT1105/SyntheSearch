9c341aa1c18aea81b410f16e65f8f51961fcb74e
hienhg <giahienhoang99@gmail.com>
2024-11-10 01:05:26 -0500

    backend-main.py: endpoint for PUT request

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from retrieve import retrieve_top_similar_results
+from crawl_data import crawl_data
+from model import create_lance_db
 
 app = FastAPI()
 
@@ -12,7 +14,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Allows your React app to make requests
+    allow_origins=["*"],  # Allows your React app to make requests
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
@@ -21,12 +23,22 @@ app.add_middleware(
 class MessageRequest(BaseModel):
     message: str
 
+class CategoryInput(BaseModel):
+    enteredKeyword: str
+
+@app.put("/put-category")
+async def put_category(category: CategoryInput):
+    """
+    Call crawl_data() with request message as input.
+    This PUT method does not return a response body.
+    """
+    crawl_data(category.enteredKeyword)
+    create_lance_db()
+    return {"message": "raw_data crawled and converted into vectors in LanceDB"}
+
+
 @app.post("/message")
 async def receive_message(request: MessageRequest):
-    # # Here, you can process the message and generate a response
-    # print(f"Received message: {request.message}")
-    # # For simplicity, we'll return a simple response
-    # return {"reply": f"Message received: {request.message}"}
     # Call the function with the input message
     results = retrieve_top_similar_results(request.message)
 
